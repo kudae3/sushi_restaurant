ec0eb3d32ac1b8f71d6253ae0e566f0925714c67
Kudae Sithu <kudaesithu2@gmail.com>
2026-05-21 09:43:19 +0630

    Implement IntroPage and update main app structure

---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 106, 23, 23),
+      body: Column(children: [
+        // shop name
+
+        // icon
+
+        // title
+
+        // subtitle
+
+        // get started button
+      ],)
+    );
+  }
+}
