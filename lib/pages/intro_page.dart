5b00eecd232236690915b26e3ceec6ea30a0d972
Kudae Sithu <kudaesithu2@gmail.com>
2026-05-25 16:45:07 +0630

    Add sushi images, update intro page layout, and configure Flutter settings

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class IntroPage extends StatelessWidget {
   const IntroPage({super.key});
@@ -6,18 +7,29 @@ class IntroPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 106, 23, 23),
-      body: Column(children: [
-        // shop name
-
-        // icon
-
-        // title
-
-        // subtitle
-
-        // get started button
-      ],)
+      backgroundColor: const Color.fromARGB(255, 78, 18, 18),
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Column( 
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 50),
+            Text('SUSHI MAN', style: GoogleFonts.dmSerifDisplay(
+              fontSize: 28,
+              color: Colors.white
+            )),
+            
+            // icon
+            SizedBox(height: 50),
+            Center(child: Image.asset('lib/images/sushi.png', width: 200, height: 200)),
+          
+            // title
+          
+            // subtitle
+          
+            // get started button
+          ],),
+      )
     );
   }
 }
