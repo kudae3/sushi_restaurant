9f9e52e9cef3e4367e4065d24703a795fa780807
Kudae Sithu <kudaesithu2@gmail.com>
2026-05-26 09:53:55 +0630

    Add onTap functionality to MyButton component and update IntroPage to use it

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -2,25 +2,29 @@ import 'package:flutter/material.dart';
 
 class MyButton extends StatelessWidget {
   final String text;
-  const MyButton({super.key, required this.text});
+  final VoidCallback onTap;
+  const MyButton({super.key, required this.text, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 60,
-      decoration: BoxDecoration(
-        color: Color.fromARGB(255, 151, 50, 50),
-        borderRadius: BorderRadius.circular(25),
-      ),
-      child: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(text, style: TextStyle(color: Colors.white, fontSize: 16)),
-            SizedBox(width: 10),
-            Icon(Icons.arrow_forward, color: Colors.white),
-          ],
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: double.infinity,
+        height: 60,
+        decoration: BoxDecoration(
+          color: Color.fromARGB(255, 151, 50, 50),
+          borderRadius: BorderRadius.circular(25),
+        ),
+        child: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(text, style: TextStyle(color: Colors.white, fontSize: 16)),
+              SizedBox(width: 10),
+              Icon(Icons.arrow_forward, color: Colors.white),
+            ],
+          ),
         ),
       ),
     );
