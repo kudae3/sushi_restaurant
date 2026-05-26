726758f157a0b80aaa9ec9f9fd61d6ca538dc57c
Kudae Sithu <kudaesithu2@gmail.com>
2026-05-26 11:48:51 +0630

    Update MenuPage layout with promotional banner and search bar; define secondaryColor in colors.dart

--- lib/pages/menu_page.dart
+++ lib/pages/menu_page.dart
@@ -13,39 +13,58 @@ class _MenuPageState extends State<MenuPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 212, 202, 202),
-      appBar: AppBar(leading: Icon(Icons.menu), backgroundColor: Colors.transparent, elevation: 0, title: Text('Menu'), centerTitle: true,),
-      body: Column(children: [
-        // promo banner
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 30.0),
-            margin: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: primaryColor,
-              borderRadius: BorderRadius.circular(25),
-            ), 
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Get 20% Off Promotion!', style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold, fontFamily: 'DMSerifDisplay')),
-                    SizedBox(height: 20),
-                    MyButton(text: 'Redeem', onTap: () => Navigator.pushNamed(context, '/intro')),
-                  ],
-                ),
-                SizedBox(width: 20),
-                Image.asset('lib/images/sushi_2.png', width: 100, height: 100),
-              ],
-            )
+        backgroundColor: const Color.fromARGB(255, 212, 202, 202),
+        appBar: AppBar(leading: Icon(Icons.menu), backgroundColor: Colors.transparent, elevation: 0, title: Text('Menu'), centerTitle: true,),
+        body: Column(children: [
+          // promo banner
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 30.0),
+              margin: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: primaryColor,
+                borderRadius: BorderRadius.circular(25),
+              ), 
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Get 20% Off Promotion!', style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold, fontFamily: 'DMSerifDisplay')),
+                      SizedBox(height: 20),
+                      MyButton(text: 'Redeem', onTap: () => Navigator.pushNamed(context, '/intro')),
+                    ],
+                  ),
+                  SizedBox(width: 20),
+                  Image.asset('lib/images/sushi_2.png', width: 100, height: 100),
+                ],
+              )
+            ),
+          
+          // search bar
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Search for sushi...',
+                prefixIcon: Icon(Icons.search),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(25),
+                    borderSide: BorderSide(color: primaryColor, width: 2),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(25),
+                    borderSide: BorderSide(color: secondaryColor, width: 2),
+                  ),
+              ),
+            ),
           ),
-        // search bar
-
-        // menu list
-
-        // popular items
-      ],)
-    );
+      
+      
+          // menu list
+      
+          // popular items
+        ],)
+      );
   }
 }
--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
 var primaryColor = Color.fromARGB(255, 78, 18, 18);
+var secondaryColor = Color.fromARGB(255, 144, 56, 56);
