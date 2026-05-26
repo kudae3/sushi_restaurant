659207205b97ae257449e973ed14f049f35562ea
Kudae Sithu <kudaesithu2@gmail.com>
2026-05-26 09:55:49 +0630

    Fix type declaration for onTap in MyButton component

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class MyButton extends StatelessWidget {
   final String text;
-  final VoidCallback onTap;
+  final void Function() onTap;
   const MyButton({super.key, required this.text, required this.onTap});
 
   @override
