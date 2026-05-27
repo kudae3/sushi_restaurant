42f2598213cb43de6d56f645601b9e14b7a60a42
Kudae Sithu <kudaesithu2@gmail.com>
2026-05-27 14:11:00 +0630

    Add price and quantity controls to DetailPage with Add to Cart button

--- lib/components/detail_page.dart
+++ lib/components/detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sushi_restaurant/components/button.dart';
 import 'package:sushi_restaurant/theme/colors.dart';
 import 'package:sushi_restaurant/models/food.dart';
 
@@ -55,6 +56,44 @@ class _DetailPageState extends State<DetailPage> {
           ),
         ),
       ),
+
+      // Price + Quantity + Add to Cart Button
+      bottomNavigationBar: Container(
+        color: primaryColor,
+        padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 20.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                // Price
+                Text(widget.food.price, style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, fontFamily: 'DMSerifDisplay', color: Colors.white)),
+                
+                // Quantity
+                Row(
+                  children: [
+                    // Derease Button
+                    Icon(Icons.remove, color: Colors.white, fontWeight: FontWeight.bold),
+                    SizedBox(width: 20),
+            
+                    // Number
+                    Text('1', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, fontFamily: 'DMSerifDisplay', color: Colors.white)),
+                    SizedBox(width: 20),
+            
+                    // Increase Button
+                    Icon(Icons.add, color: Colors.white, fontWeight: FontWeight.bold),
+                  ]
+                )
+              ],
+            ),
+            SizedBox(height: 20),
+            MyButton(text: 'Add to Cart', onTap: () => Navigator.pushNamed(context, '/cart')),
+          ],
+        ),
+      ),
+
+
     );
   }
 }
